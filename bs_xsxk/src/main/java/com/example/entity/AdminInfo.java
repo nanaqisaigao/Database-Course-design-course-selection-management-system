a048390076cef13da0fb3d80c1aa67c744371d97
nanaqisaigao <2804401992@qq.com>
2023-08-05 16:51:32 +0800

    1

--- bs_xsxk/src/main/java/com/example/entity/AdminInfo.java
+++ bs_xsxk/src/main/java/com/example/entity/AdminInfo.java
@@ -6,4 +6,12 @@ import javax.persistence.*;
 public class AdminInfo extends Account {
     @Column(name = "phone")
     private String phone;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
--- bs_xsxk/src/main/java/com/example/service/AdmininfoService.java
+++ bs_xsxk/src/main/java/com/example/service/AdmininfoService.java
@@ -1,8 +1,10 @@
 package com.example.service;
 
+import com.example.dao.AdminInfoDao;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdmininfoService {
+    private AdminInfoDao adminInfoDao;
 
 }
